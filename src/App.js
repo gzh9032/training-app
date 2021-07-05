6fb259ecc73c7523e670a8c7c8baa1e699683651
gzh9032 <840170720@qq.com>
2021-07-05 17:59:11 +0800

    my app

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import
+React
+    from
+    'react';
+
+
+
+
+
+
+class
+    App
+    extends
+    React.
+        Component {
+
+
+
+
+    render() {
+
+
+
+
+        return (
+
+
+            <   div  >      
+                
+                A simple personal website
+                 
+            </  div  >
+
+          
+
+        )
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+export
+    default
+    App;
